1a1b9112a25e9042eacbc03b1707ab1dfb890a69
Felipe Machado <felipe.machado1992@hotmail.com>
2017-09-06 03:26:34 -0300

    retirada de campos que foram para a sessão nova meradoria

--- TestewebValemobi/index.php
+++ TestewebValemobi/index.php
@@ -1,36 +1,33 @@
 <!DOCTYPE html>
 <!--
-    Página inicial onde o usuário poderá cadastrar nova movimentação
+    Página inicial onde o usuário poderá cadastrar nova negociação
 -->
 <html>
     <head>
         <meta charset="UTF-8">
         <title></title>
+        
     </head>
     <body>
         <form method="get" action="gravar.php">
-	<label>ID da mercadoria</label><br>
-	<input type="number" min="1" name="id"><br>
-        
-	<label>Tipo da mercadoria</label><br>
-	<input type="text" name="tipoMerc"><br>
-        
-	<label>Nome da mercadoria</label><br>
-	<input type="text" name="nome"><br>
-        
-	<label>Quantidade da mercadoria</label><br>
-	<input type="number" min="1" name="quantidade"><br>
-        
-	<label>Preço</label><br>
-	<input type="text" name="preco"><br>
-        
-	<label>Tipo de negócio</label><br>
-	<select class="form-control" name="tipoNeg">
-                            <option value="compra">Compra</option>
-                            <option value="venda">Venda</option>
-	</select><br>
-        
-	<input class="btn btn-default" type="submit" value="Salvar">
+            <label>ID da mercadoria</label><br>
+            <input type="number" min="1" name="id">
+            
+            <input type="button" value="Pesquisar" onclick="pesquisar()"><br>
+            
+            <label>Quantidade da mercadoria</label><br>
+            <input type="number" min="1" name="quantidade"><br>
+            
+            <label>Valor Total</label><br>
+            <input type="text" name="total"><br>
+            
+            <label>Tipo de negócio</label><br>
+            <select class="form-control" name="tipoNeg">
+                <option value="compra">Compra</option>
+                <option value="venda">Venda</option>
+            </select><br>
+            
+            <input class="btn btn-default" type="submit" value="Salvar">
         </form>
     </body>
 </html>
