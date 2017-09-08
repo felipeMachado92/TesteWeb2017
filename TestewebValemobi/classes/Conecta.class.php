f5b858597f9b15b2f7072feb697e3edf399dbf42
Felipe Machado <felipe.machado1992@hotmail.com>
2017-09-08 01:41:22 -0300

    Implementação da classe conecta

--- TestewebValemobi/classes/Conecta.class.php
+++ TestewebValemobi/classes/Conecta.class.php
@@ -1,16 +1,32 @@
 <?php
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
- * Description of Conecta
+ * Classe criada para gerar conexão com o banco de dados
  *
  * @author Felipe
  */
 class Conecta {
-    //put your code here
+        private $usuario;
+        private $senha;
+        private $banco;
+        private $servidor;
+        private static $pdo;
+        
+        function __construct() {
+            $this->usuario = "root";
+            $this->senha = "";
+            $this->banco = "testeweb";
+            $this->servidor = "localhost";
+        }
+        
+        public function conectar() {
+            try{
+                if(is_null(self::$pdo)){
+                    self::$pdo = new PDO("mysql:host=".$this->servidor.";dbname=".$this->banco, $this->usuario, $this->senha);
+                }
+                return self::$pdo;
+            } catch (PDOException $ex) {
+                echo $ex->getMessage();
+            }
+        }
 }
