ccd808e0f922714d1bfebf6269580072f3075e40
Felipe Machado <felipe.machado1992@hotmail.com>
2017-09-08 03:01:21 -0300

    Implementação da classe mercadoria

--- TestewebValemobi/classes/Mercadoria.class.php
+++ TestewebValemobi/classes/Mercadoria.class.php
@@ -5,55 +5,56 @@
  *
  * @author Felipe
  */
+
+require_once 'Conecta.class.php';
 class Mercadoria {
     private $cdMercadoria;
     private $tpMercadoria;
     private $nmMercadoria;
     private $vlMercadoria;
     
-    //Construtor
-    function __construct($cdMercadoria, $tpMercadoria, $nmMercadoria, $vlMercadoria) {
-        $this->cdMercadoria = $cdMercadoria;
-        $this->tpMercadoria = $tpMercadoria;
-        $this->nmMercadoria = $nmMercadoria;
-        $this->vlMercadoria = $vlMercadoria;
+    public function __construct(){
+        $this->con = new Conexao();
     }
     
-    //Inicio Getters e Setters
-    function getCdMercadoria() {
-        return $this->cdMercadoria;
-    }
-
-    function getTpMercadoria() {
-        return $this->tpMercadoria;
-    }
-
-    function getNmMercadoria() {
-        return $this->nmMercadoria;
-    }
-
-    function getVlMercadoria() {
-        return $this->vlMercadoria;
-    }
-
-    function setCdMercadoria($cdMercadoria) {
-        $this->cdMercadoria = $cdMercadoria;
-    }
-
-    function setTpMercadoria($tpMercadoria) {
-        $this->tpMercadoria = $tpMercadoria;
-    }
-
-    function setNmMercadoria($nmMercadoria) {
-        $this->nmMercadoria = $nmMercadoria;
-    }
-
-    function setVlMercadoria($vlMercadoria) {
-        $this->vlMercadoria = $vlMercadoria;
+    public function __set($atributo, $valor){
+        $this->$atributo = $valor;
     }
     
-    //Fim Getters e Setters
+    public function __get($atributo){
+        return $this->$atributo;
+    }
     
-
+        public function queryInsert($dados){
+        try{
+            $this->cdMercadoria = $dados['cdMercdoria'];
+            $this->nmMercadoria = $dados['nmMercadoria'];
+            $this->tpMercadoria = $dados['tpMercadoria'];
+            $this->vlMercadoria = $dados['vlMercadoria'];
+            $cst = $this->con->conectar()->prepare("INSERT INTO `tb_mercadoria` (`cd_mercadoria`, `nm_mercadoria`, `tp_mercadoria`, `vl_mercadoria`) VALUES (:cdMercdoria, :nmMercadoria, :tpMercadoria, :vlMercadoria);");
+            $cst->bindParam(":cdMercadoria", $this->cdMercadoria, PDO::PARAM_INT);
+            $cst->bindParam(":nmMercadoria", $this->nmMercadoria, PDO::PARAM_STR);
+            $cst->bindParam(":tpMercadoria", $this->tpMercadoria, PDO::PARAM_STR);
+            $cst->bindParam(":vlMercadoria", $this->vlMercadoria, PDO::PARAM_STR);
+            if($cst->execute()){
+                return 'ok';
+            }else{
+                return 'erro';
+            }
+        } catch (PDOException $ex) {
+            return 'error '.$ex->getMessage();
+        }
+    }
+    
+        public function querySeleciona($dado){
+        try{
+            $cst = $this->con->conectar()->prepare("SELECT cd_mercadoria, nm_mercadoria, tp_mercadoria, vl_mercadoria FROM `tb_mercadoria` WHERE `cd_mercadoria` = :cdMercadoria;");
+            $cst->bindParam(":cdMercadoria", $this->cdMercadoria, PDO::PARAM_INT);
+            $cst->execute();
+            return $cst->fetch();
+        } catch (PDOException $ex) {
+            return 'error '.$ex->getMessage();
+        }
+    }
 }
 
