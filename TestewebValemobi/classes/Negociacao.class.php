09ac08278cc1ee540b1309c3eb8b8095a17b66bd
Felipe Machado <felipe.machado1992@hotmail.com>
2017-09-08 20:10:25 -0300

    Criação dos métodos insert do banco de dados

--- TestewebValemobi/classes/Negociacao.class.php
+++ TestewebValemobi/classes/Negociacao.class.php
@@ -5,53 +5,46 @@
  *
  * @author Felipe
  */
+require_once 'Conecta.class.php';
+require_once 'Mercadoria.class.php';
 class Negociacao {
+    private $con;
     private $mercadoria;
     private $qtdMercadoria;
     private $tpNegociacao;
     private $vlNegociacao;
     
-    //Construtor da classe
-    function __construct($mercadoria, $qtdMercadoria, $tpNegociacao, $vlNegociacao) {
-        $this->mercadoria = $mercadoria;
-        $this->qtdMercadoria = $qtdMercadoria;
-        $this->tpNegociacao = $tpNegociacao;
-        $this->vlNegociacao = $vlNegociacao;
+    public function __construct(){
+        $this->con = new Conexao();
     }
     
-    //Inicio Getters e Setters
-    function getMercadoria() {
-        return $this->mercadoria;
-    }
-
-    function getQtdMercadoria() {
-        return $this->qtdMercadoria;
-    }
-
-    function getTpNegociacao() {
-        return $this->tpNegociacao;
-    }
-
-    function getVlNegociacao() {
-        return $this->vlNegociacao;
-    }
-
-    function setMercadoria($mercadoria) {
-        $this->mercadoria = $mercadoria;
-    }
-
-    function setQtdMercadoria($qtdMercadoria) {
-        $this->qtdMercadoria = $qtdMercadoria;
-    }
-
-    function setTpNegociacao($tpNegociacao) {
-        $this->tpNegociacao = $tpNegociacao;
-    }
-
-    function setVlNegociacao($vlNegociacao) {
-        $this->vlNegociacao = $vlNegociacao;
+    public function __set($atributo, $valor){
+        $this->$atributo = $valor;
     }
     
-    //Inicio Getters e Setters
+    public function __get($atributo){
+        return $this->$atributo;
+    }
+    
+    public function queryInsert($dados){
+        try{
+            $this->qtdMercadoria = $dados['qtdMercadoria'];
+            $this->nmMercadoria = $dados['nmMercadoria'];
+            $this->tpNegociacao = $dados['tpNegociacao'];
+            $this->vlNegociacao = $dados['vlNegociacao'];
+            $cst = $this->con->conectar()->prepare("INSERT INTO `tb_mercadoria` (`cd_mercadoria`, `nm_mercadoria`, `tp_mercadoria`, `vl_mercadoria`) VALUES (:cdMercdoria, :nmMercadoria, :tpMercadoria, :vlMercadoria);");
+            $cst->bindParam(":cdMercadoria", $this->cdMercadoria, PDO::PARAM_INT);
+            $cst->bindParam(":nmMercadoria", $this->nmMercadoria, PDO::PARAM_STR);
+            $cst->bindParam(":tpMercadoria", $this->tpMercadoria, PDO::PARAM_STR);
+            $cst->bindParam(":vlMercadoria", $this->vlMercadoria, PDO::PARAM_STR);
+            if($cst->execute()){
+                return 'ok';
+            }else{
+                return 'erro';
+            }
+        } catch (PDOException $ex) {
+            return 'error '.$ex->getMessage();
+        }
+    }
     
 }
