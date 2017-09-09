7094249d38964c7a443b5b7c163ca644b19383b9
Felipe Machado <felipe.machado1992@hotmail.com>
2017-09-09 14:24:56 -0300

    Implementação do insert

--- TestewebValemobi/novaMercadoria.php
+++ TestewebValemobi/novaMercadoria.php
@@ -2,26 +2,40 @@
 <!--
 Página onde o usuário poderá cadastrar nova mercadoria
 -->
+<?php 
+     require_once 'classes/Mercadoria.class.php';
+     
+     $merc = new Mercadoria();
+     
+     if(isset($_POST['btSalvar'])) {
+         if($merc->queryInsert($_POST) == 'ok'){
+             header('location:index.php');
+         } else {
+             echo '<script type="text/javascript">alert("Erro em cadastrar");</script>';
+         }
+     }   
+    
+?>
 <html>
     <head>
         <meta charset="UTF-8">
         <title></title>
     </head>
     <body>
-        <form>
-            <label>ID da mercadoria</label><br>
-            <input type="number" min="1" name="id"><br>
+        <form name="form" action="" method="post" >
+            <label>Código da mercadoria</label><br>
+            <input type="number" min="1" name="cdMercdoria"><br>
 
             <label>Tipo da mercadoria</label><br>
-            <input type="text" name="tipoMerc"><br>
+            <input type="text" name="tpMercadoria"><br>
 
             <label>Nome da mercadoria</label><br>
-            <input type="text" name="nome"><br>
+            <input type="text" name="nmMercadoria"><br>
             
             <label>Preço</label><br>
-            <input type="text" name="preco"><br>
+            <input type="text" name="vlMercadoria"><br>
             
-            <input class="btn btn-default" type="submit" value="Salvar">
+            <input type="submit" value="Salvar" name="btSalvar">
         </form>
     </body>
 </html>
